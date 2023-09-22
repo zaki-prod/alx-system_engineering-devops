d6f61ff5d083b50735cccd62d67b680389142da5
zaki-prod <zakiprod04@gmail.com>
2023-09-22 08:52:54 -0700

    2-execute_a_command.pp

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,6 @@
 #This code  creates a manifest that kills a process named killmenow
- exec{'killmenow':
-   command  => '/ust/bin/pkill killmenow',
+ exec{ 'killmenow':
+   command => '/usr/bin/pkill killmenow',
    provider => 'shell',
    returns  => [0, 1],
  }
