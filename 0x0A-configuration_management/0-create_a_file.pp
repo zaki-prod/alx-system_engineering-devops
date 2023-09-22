76fbbe81886f7ba2c2f972dfe5c9e6bd62ba9f76
zaki-prod <zakiprod04@gmail.com>
2023-09-22 08:33:43 -0700

    0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,9 @@
 # this code creates a file in /tmp
-file{ '/tmp/schhol':
+file{ '/tmp/school':
   ensure => file,
   content => 'I love Puppet',
-  mode => '07744',
-  owner => 'www-data',
-  group =>  'www-data',
+  mode    => '07744',
+  owner   => 'www-data',
+  group   =>  'www-data',
 }
+
