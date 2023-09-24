67722c94a27cc101fb3a3e5699013e7825ff5090
zaki-prod <zakiprod04@gmail.com>
2023-09-24 15:03:18 -0700

    100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,14 +1,14 @@
 #!/usr/bin/bash/env bash
 #using puppet to make changes to our cinfiguration file
 
-fiel { 'sct/ssh/ssh_config':
+file { 'ect/ssh/ssh_config':
 	ensure => present,
 
 content =>"
 
 	#ssh client configuration
 	host*
-	IdentifyFile !/.ssh/schhol
-	paaswordAuthentication no
+	IdentifyFile ~/.ssh/schhol
+	passwordAuthentication no
 
 }
