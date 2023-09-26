d994c0ac02b0d9e905753662a19a5cbf06bac775
zaki-prod <zakiprod04@gmail.com>
2023-09-26 13:52:32 -0700

    Task 4 Advanced

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,14 +1,16 @@
 #!/usr/bin/bash/env bash
-#using puppet to make changes to our cinfiguration file
+# using puppet to make changes to our cinfiguration file
 
-file { 'ect/ssh/ssh_config':
+file { '/etc/ssh/ssh_config':
 	ensure => present,
 
 content =>"
 
-	#ssh client configuration
+	# ssh client configuration
+	
 	host*
 	IdentifyFile ~/.ssh/school
 	passwordAuthentication no
+	"
 
 }
